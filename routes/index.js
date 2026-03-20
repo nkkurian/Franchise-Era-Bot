32cc0894f13496eb71d9b214344879ced2fd6f28
nkkurian <160970395+nkkurian@users.noreply.github.com>
2026-03-20 13:11:00 -0400

    Create index.js

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// Import individual report files
+const faReport = require('./fa-report');
+const extReport = require('./ext-report');
+// const capReport = require('./cap-report'); // Add this later!
+
+module.exports = (client, doc) => {
+    // Tell the router to use these files
+    router.use('/', faReport(client, doc));
+    router.use('/', extReport(client, doc));
+
+    return router;
+};
