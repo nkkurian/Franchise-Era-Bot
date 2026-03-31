04eb309fe25aae84a0a02327651b47e7e62e1f44
nkkurian <160970395+nkkurian@users.noreply.github.com>
2026-03-31 10:30:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ client.once('ready', async () => {
         // 2. Delay Startup Tasks to avoid Discord Rate Limits (429 errors)
         setTimeout(async () => {
             await sendStartupTestMessage();
-            await initializeData();
+            //await initializeData();
             
             // Check Discord Connection Status
             if (client.ws.status !== 0) {
@@ -276,9 +276,9 @@ async function initializeData() {
         console.log("📥 Loading Sleeper Team Map...");
         await updateTeamMap(); 
 
-        console.log("🔍 Starting Sleeper Poller...");
-        await pollSleeper(); // Run once
-        setInterval(pollSleeper, 60000); // Then every minute
+        // console.log("🔍 Starting Sleeper Poller...");
+        // await pollSleeper(); // Run once
+        // setInterval(pollSleeper, 60000); // Then every minute
     } catch (err) {
         console.error("❌ Background Init Error:", err);
     }
