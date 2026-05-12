0dddd9b32a5818aa792b6e958ccb672d10452e12
nkkurian <160970395+nkkurian@users.noreply.github.com>
2026-05-12 10:40:05 -0400

    Update salary.js

--- commands/salary.js
+++ commands/salary.js
@@ -9,7 +9,7 @@ module.exports = {
 
     async execute(interaction, getSheetData, getPlayerStats) {
         console.log("DEBUG: Salary command started"); // Checkpoint 1
-        await interaction.deferReply();
+        //await interaction.deferReply();
       const input = interaction.options.getString("player").toLowerCase();
         console.log("DEBUG: Fetching sheet data..."); // Checkpoint 2
             const { players, logs, idMap } = await getSheetData();
